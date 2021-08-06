0f3e2bafc0463cc386c4e69cd89bc4ea01e49316
James Kinda <jameskinda01@gmail.com>
2021-08-05 20:39:16 -0700

    master

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Resume from "./components/Resume";
 import { Route } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import image from "./coffee-apple.jpg";
 import Portfolio from "./components/Portfolio";
 import Contacts from "./components/Contacts";
-import backgroundImage from "./pictures/fiber.jpg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
